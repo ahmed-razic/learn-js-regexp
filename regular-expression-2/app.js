31b4303978db0e04d4b7b466bc67640ed7fbdb1a
ahmed-razic <ahmed.razic@gmail.com>
2022-04-07 12:14:17 +0200

    reg-exp-2

--- regular-expression-2/app.js
+++ regular-expression-2/app.js
@@ -9,8 +9,21 @@ re = /h.llo/i;
 re = /h*llo/i;
 re = /hel?b?lo/i;
 re = /hello\?/i;
+re = /he[ll]o/i;
 
-const str = 'Hello?';
+re = /gr[ae]y/i;
+re = /[GFK]ray/;
+re = /[^GFK]ray/;
+re = /[A-Z]ray/;
+re = /[0-9]ray/;
+
+re = /Hel{2}o/i;
+re = /Hel{2-4}o/i;
+re = /Hel{2,}o/i;
+
+re = /([0-9]x){3}/i;
+
+const str = '3x3x3x';
 
 const result = re.exec(str);
 console.log(result);
